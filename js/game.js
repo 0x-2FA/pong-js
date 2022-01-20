36af3aad5e4926c0e2abb5a62982aad898107cb6
0x-2FA <2fa_gh@protonmail.com>
2022-01-20 13:07:43 +0200

    Created a player paddle

---
+++ js/game.js
@@ -0,0 +1,14 @@
+import Paddle from "./paddle.js";
+
+const canvas = document.getElementById("canvas");
+
+canvas.width = 900;
+canvas.height = 500;
+
+const context = canvas.getContext("2d");
+
+const color_whitish = "#F0EBD8";
+
+const player = new Paddle(20, 250, 5, 70, color_whitish);
+
+player.create(context);
