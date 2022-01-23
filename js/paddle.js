bf92bf484e120fcd4c8c46c0d361ef7b373e6b22
0x-2FA <2fa_gh@protonmail.com>
2022-01-23 22:37:37 +0200

    Changed a bit how the draw and update functions work

--- js/paddle.js
+++ js/paddle.js
@@ -1,24 +1,31 @@
 export default class Paddle
 {
   
-  constructor(x, y, width, height, color)
+  constructor(x, y, width, height, color, speed = 4)
   {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.color = color;
+    this.speed = speed
   }
   
   draw(context)
   {
     context.fillStyle = this.color;
-    context.fillRect(this.x, this.y - (this.height / 2), this.width, this.height);
-    
+    context.fillRect(this.x, this.y, this.width, this.height);
   }
 
-  update(context)
+  update(dt)
   {
-    this.draw(context);
+    if (!dt) {return;}
+    this.y -= this.speed;
+  }
+
+  reset(context)
+  {
+    context.fillStyle = this.color;
+    context.fillRect(this.x, 250 - (this.height / 2), this.width, this.height);
   }
 }
