77c4a1057cabc71ffc06e0a7f196652f03ba4c3b
0x-2FA <2fa_gh@protonmail.com>
2022-01-23 23:53:40 +0200

    Restructured to a more OOP style

--- js/paddle.js
+++ js/paddle.js
@@ -18,37 +18,4 @@ export default class Paddle
     context.fillStyle = this.color;
     context.fillRect(this.x, this.y, this.width, this.height);
   }
-
-  update(dt)
-  {
-    if (!dt) {return;}
-    
-    this.y += this.velocity;
-
-    if (this.y < 0) 
-    {
-      this.y = 0;  
-    }
-
-    if (this.y + this.height > this.ch) 
-    {
-      this.y = this.ch - this.height;
-    }
-  }
-
-  up()
-  {
-    this.velocity = -this.speed;
-  }
-
-  down()
-  {
-    this.velocity = this.speed;
-  }
-
-  reset()
-  {
-    this.y = 250 - (this.height / 2);
-  }
-
 }
---
+++ js/player.js
@@ -0,0 +1,42 @@
+import Paddle from "./paddle.js"
+export default class Player extends Paddle 
+{
+  super(){}
+
+  update(dt)
+  {
+    if (!dt) {return;}
+    
+    this.y += this.velocity;
+
+    if (this.y < 0) 
+    {
+      this.y = 0;  
+    }
+
+    if (this.y + this.height > this.ch) 
+    {
+      this.y = this.ch - this.height;
+    }
+  }
+
+  reset()
+  {
+    this.y = 250 - (this.height / 2);
+  }
+
+  up()
+  {
+      this.velocity = -this.speed;
+  }
+
+  down()
+  {
+      this.velocity = this.speed;
+  }
+
+  stop()
+  {
+      this.velocity = 0;
+  }
+}
